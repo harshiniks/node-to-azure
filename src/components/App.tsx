ed2a0efd8cd355c4b04f4c120204c6b437c18dfd
Harshini K S <harshiniks@github.com>
2021-10-14 16:54:34 +0530

    Nice UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,15 +2,53 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    const style = {
-        fontSize: 35,
-        marginTop: '20%',
-        marginLeft: '30%',
-        background: 'aqua',
-        marginRight: '20%',
-      };
     return (
-      <div style={style}>GitHub Student Developer, Here is your helloworld!!!</div>
+    <div className="main-container">
+        <div className="cloud-image">
+            <img alt="" src="./img/successCloudNew.svg" />
+        </div>
+        <div className="content">
+            <div className="tweet-container">
+            <a href="http://twitter.com/intent/tweet/?text=I%20just%20created%20a%20new%20Node.js%20website%20on%20Azure%20using%20GitHub%20workflow&hashtags=GitHub%2CGitHubWorkflow%20%40Azure">
+                <img src="img/tweetThis.svg" />
+            </a>
+        </div>
+            <div className="content-body">
+                <div className="success-text">Github Hackathon!</div>
+                <div className="description line-1"> Congratulations on choosing awesome github education packs</div>
+                <div className="description line-2"> Your <b>REACT APP</b> is up and running on Azure</div>
+                <div className="next-steps-container">
+                    <div className="next-steps-header">Next up</div>
+                    <div className="next-steps-body">
+                        <div className="step">
+                            <div className="step-icon">
+                                <img src="img/cloneWhite.svg" />
+                            </div>
+                            <div className="step-text"><a href="https://help.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository">Clone your code repository and start developing your application on IDE of your choice</a></div>
+                        </div>
+                        <div className="step">
+                            <div className="step-icon">
+                                <img src="img/deployWhite.svg" />
+                            </div>
+                            <div className="step-text"><a href="https://help.github.com/en/actions">View your workflow on GitHub and customize it as per your needs</a></div>
+                        </div>
+                        <div className="step">
+                            <div className="step-icon">
+                                <img src="img/stackWhite.svg" />
+                            </div>
+                            <div className="step-text"><a href="http://portal.azure.com">View your service stack in the Azure Portal</a></div>
+                        </div>
+                        <div className="step">
+                            <div className="step-icon">
+                                <img src="img/lightbulbWhite.svg" />
+                            </div>
+                            <div className="step-text"><a href="https://go.microsoft.com/fwlink/?linkid=862126">Learn more about all you can do with GitHub workflow by visiting the documentation</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     );
   }
 }
